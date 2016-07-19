06529b8f939f2f0136efc5029227e53abfca00d2
junzis <junzis@users.noreply.github.com>
2016-07-19 12:13:23 +0200

    add readthedocs config file

--- conf.py
+++ conf.py
@@ -222,7 +222,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'adsb-decode-guide.tex', u'ADS-B Decoding Guide Documentation',
+  (master_doc, 'adsb-decode-guide.tex', u'ADS-B Decoding Guide',
    u'Junzi Sun', 'manual'),
 ]
 
@@ -252,7 +252,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide Documentation',
+    (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide',
      [author], 1)
 ]
 
@@ -266,7 +266,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide Documentation',
+  (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide',
    author, 'adsb-decode-guide', 'One line description of project.',
    'Miscellaneous'),
 ]
