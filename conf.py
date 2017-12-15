74cdf030979c17043c1c25a2c0e32cedff3409ae
Junzi Sun <junzisun@gmail.com>
2017-12-15 15:47:35 +0100

    2to3

--- conf.py
+++ conf.py
@@ -224,8 +224,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'adsb-decode-guide.tex', u'ADS-B Decoding Guide',
-   u'Junzi Sun', 'manual'),
+  (master_doc, 'adsb-decode-guide.tex', 'ADS-B Decoding Guide',
+   'Junzi Sun', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide',
+    (master_doc, 'adsb-decode-guide', 'ADS-B Decoding Guide',
      [author], 1)
 ]
 
@@ -268,7 +268,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'adsb-decode-guide', u'ADS-B Decoding Guide',
+  (master_doc, 'adsb-decode-guide', 'ADS-B Decoding Guide',
    author, 'adsb-decode-guide', 'One line description of project.',
    'Miscellaneous'),
 ]
