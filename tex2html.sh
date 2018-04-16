14bdb51e846cd9339d17224648f1a11f993cb1c0
Junzi Sun <junzisun@gmail.com>
2018-04-16 13:11:37 +0200

    add "edit on github" function

--- tex2html.sh
+++ tex2html.sh
@@ -1,39 +1,42 @@
-for file in index adsb ehs
+for fn in index adsb ehs
 do
-  pandoc $file.tex \
-    --output _build/html/$file.html \
+  pandoc $fn.tex \
+    --output _build/html/$fn.html \
     --template _static/template.html5 \
     --css bootstrap.min.css \
     --css style.css \
     --toc \
     --toc-depth=3 \
-    --mathjax
+    --mathjax \
+    --variable editat=$fn
 done
 
 for file in adsb/*.tex
 do
-  fbase=${file%.tex}
+  fn=${file%.tex}
   pandoc $file \
-    --output _build/html/$fbase.html \
+    --output _build/html/$fn.html \
     --template _static/template.html5 \
     --css bootstrap.min.css \
     --css style.css \
     --toc \
     --toc-depth=3 \
     --mathjax \
-    --variable rootdir='../'
+    --variable rootdir="../" \
+    --variable editat=$fn
 done
 
 for file in ehs/*.tex
 do
-  fbase=${file%.tex}
+  fn=${file%.tex}
   pandoc $file \
-    --output _build/html/$fbase.html \
+    --output _build/html/$fn.html \
     --template _static/template.html5 \
     --css bootstrap.min.css \
     --css style.css \
     --toc \
     --toc-depth=3 \
     --mathjax \
-    --variable rootdir='../'
+    --variable rootdir="../" \
+    --variable editat=$fn
 done
